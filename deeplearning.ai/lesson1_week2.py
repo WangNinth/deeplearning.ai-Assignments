6b08b8bce911c6a3337da43f2206c7f4aab214fa
xiaojiu <wangxiaojiu@tju.edu.cn>
2020-07-23 18:55:25 +0800

    d

---
+++ deeplearning.ai/lesson1_week2.py
@@ -0,0 +1,146 @@
+import numpy as np
+import matplotlib.pyplot as plt 
+import h5py
+import scipy 
+from PIL import Image
+from lr_utils import load_dataset
+
+#Loading the data
+train_set_x_orig, train_set_y, test_set_x_orig, test_set_y, classes = load_dataset()
+
+train_set_x_flatten = train_set_x_orig.reshape(train_set_x_orig.shape[0],-1).T
+test_set_x_flatten = test_set_x_orig.reshape(test_set_x_orig.shape[0],-1).T
+
+#standardize dataset
+train_set_x = test_set_x_flatten/255
+test_set_x = test_set_x_flatten/255
+
+#created sigmod function
+def sigmoid(z):
+    s = 1/(1+np.exp(-z))
+    return s
+
+#initialize
+def initialize_with_zeros(dim):
+    
+    w = np.zeros((dim,1))
+    b = 0
+
+    assert(w.shape == (dim, 1))
+    assert(isinstance(b, float) or isinstance(b, int))
+    
+    return w, b
+
+# GRADED FUNCTION: propagate
+
+def propagate(w, b, X, Y):
+    """
+    Implement the cost function and its gradient for the propagation explained above
+
+    Arguments:
+    w -- weights, a numpy array of size (num_px * num_px * 3, 1)
+    b -- bias, a scalar
+    X -- data of size (num_px * num_px * 3, number of examples)
+    Y -- true "label" vector (containing 0 if non-cat, 1 if cat) of size (1, number of examples)
+
+    Return:
+    cost -- negative log-likelihood cost for logistic regression
+    dw -- gradient of the loss with respect to w, thus same shape as w
+    db -- gradient of the loss with respect to b, thus same shape as b
+    
+    Tips:
+    - Write your code step by step for the propagation. np.log(), np.dot()
+    """
+    
+    m = X.shape[1]
+    # FORWARD PROPAGATION (FROM X TO COST)
+    ### START CODE HERE ### (≈ 2 lines of code)
+    A = sigmoid(np.dot(w.T, X)+b)
+    cost = np.sum((Y*np.log(A)+(1-Y)*np.log(1-A)),axis = 1)/(-m)
+    ### END CODE HERE ###
+    
+    # BACKWARD PROPAGATION (TO FIND GRAD)
+    ### START CODE HERE ### (≈ 2 lines of code)
+    dz = A-Y #(1,m)
+    db = np.sum((A-Y),axis=1)/m
+    dw = np.dot(X, dz.T)/m #(num_px * num_px * 3, 1)  X(num_px * num_px * 3, m)
+    ### END CODE HERE ###
+
+    assert(dw.shape == w.shape)
+    assert(db.dtype == float)
+    cost = np.squeeze(cost)
+    assert(cost.shape == ())
+    
+    grads = {"dw": dw,
+             "db": db}
+    return grads, cost
+# GRADED FUNCTION: optimize
+
+def optimize(w, b, X, Y, num_iterations, learning_rate, print_cost = False):
+    """
+    This function optimizes w and b by running a gradient descent algorithm
+    
+    Arguments:
+    w -- weights, a numpy array of size (num_px * num_px * 3, 1)
+    b -- bias, a scalar
+    X -- data of shape (num_px * num_px * 3, number of examples)
+    Y -- true "label" vector (containing 0 if non-cat, 1 if cat), of shape (1, number of examples)
+    num_iterations -- number of iterations of the optimization loop
+    learning_rate -- learning rate of the gradient descent update rule
+    print_cost -- True to print the loss every 100 steps
+    
+    Returns:
+    params -- dictionary containing the weights w and bias b
+    grads -- dictionary containing the gradients of the weights and bias with respect to the cost function
+    costs -- list of all the costs computed during the optimization, this will be used to plot the learning curve.
+    
+    Tips:
+    You basically need to write down two steps and iterate through them:
+        1) Calculate the cost and the gradient for the current parameters. Use propagate().
+        2) Update the parameters using gradient descent rule for w and b.
+    """
+    
+    costs = []
+    
+    for i in range(num_iterations):
+        
+        
+        # Cost and gradient calculation (≈ 1-4 lines of code)
+        ### START CODE HERE ### 
+        grads, cost = propagate(w, b, X, Y)
+        ### END CODE HERE ###
+        
+        # Retrieve derivatives from grads
+        dw = grads["dw"]
+        db = grads["db"]
+        
+        # update rule (≈ 2 lines of code)
+        ### START CODE HERE ###
+        w = w - learning_rate*dw
+        b = b - learning_rate*db
+        ### END CODE HERE ###
+        
+        # Record the costs
+        if i % 100 == 0:
+            costs.append(cost)
+        
+        # Print the cost every 100 training examples
+        if print_cost and i % 100 == 0:
+            print ("Cost after iteration %i: %f" %(i, cost))
+    
+    params = {"w": w,
+              "b": b}
+    
+    grads = {"dw": dw,
+             "db": db}
+    
+    return params, grads, costs
+dim = 2
+w, b = initialize_with_zeros(dim) 
+w, b, X, Y = np.array([[1.],[2.]]), 2., np.array([[1.,2.,-1.],[3.,4.,-3.2]]), np.array([[1,0,1]])
+params, grads, costs = optimize(w, b, X, Y, num_iterations= 100, learning_rate = 0.009, print_cost = False)
+
+print ("w = " + str(params["w"]))
+print ("b = " + str(params["b"]))
+print ("dw = " + str(grads["dw"]))
+print ("db = " + str(grads["db"]))
